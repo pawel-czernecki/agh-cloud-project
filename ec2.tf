464cc18ba4f7a04041d806e1abb9c089ac8cf275
Wojciech Matuszyński <wojciech@matuszynski.eu>
2025-12-01 22:32:08 +0100

    barfoo

--- ec2.tf
+++ ec2.tf
@@ -26,6 +26,7 @@ resource "aws_launch_template" "ec2_web" {
 
     rm -fr /usr/share/nginx/html/*
 
+    # multicloud approach ;) - pogoda service
     cat >/etc/nginx/conf.d/default.conf <<EOL
     server{
 
